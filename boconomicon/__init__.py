ba89052e2f348b95cc372d71f094cbf5942ec48a
Landon Holland <landon.holland02@gmail.com>
2020-11-17 16:53:45 -0500

    initial boconomicon cog code

---
+++ boconomicon/__init__.py
@@ -0,0 +1,5 @@
+from .boconomicon import Boconomicon
+
+
+def setup(bot):
+    bot.add_cog(Boconomicon())
---
+++ boconomicon/boconomicon.py
@@ -0,0 +1,106 @@
+from redbot.core import commands, checks, Config
+import discord
+
+
+class Boconomicon(commands.Cog):
+    """Boconomicon"""
+
+    def __init__(self):
+        # Default config for all guilds with random identifier
+        self.config = Config.get_conf(self, identifier=9988776655)
+        default_guild = {
+            "enabled": False,
+            "triggers": [
+                "ca",
+                "ce",
+                "ci",
+                "co",
+                "cu",
+                "cho",
+                "cha",
+                "chi"
+            ],
+            "overrides": {
+                "necronomicon": "boconomicon",
+                "economy": "boconomy",
+                "vocabulary": "bocabulary",
+                "algorithm": "boalgorithm"
+            }
+        }
+        self.config.register_guild(**default_guild)
+
+    async def generate_cache(self):
+        self.boconomicon_cache = await self.config.all_guilds()
+
+    def cog_unload(self):
+        if self.boconomicon_cache is not None:
+            self.boconomicon_cache.clear()
+
+    @commands.group(aliases=["bset"])
+    @commands.guild_only()
+    @checks.admin_or_permissions(manage_guild=True)
+    async def boconomiconset(self, ctx: commands.Context):
+        """Adjust Boconomicon settings"""
+        pass
+
+    @boconomiconset.group(name="trigger", alias=["triggers"])
+    async def boconomiconset_trigger(self, ctx: commands.Context):
+        """Add or remove triggers for Boconomicon"""
+        if not ctx.invoked_subcommand:
+            embed = discord.Embed(color=await ctx.embed_color(), title="Current triggers")
+            msg = ""
+            triggers = await self.config.guild(ctx.guild).triggers()
+            for trigger in triggers:
+                msg += trigger + "\n"
+            embed.description = msg
+            await ctx.send(embed=embed)
+
+    @boconomiconset_trigger.command(name="add")
+    async def boconomiconset_trigger_add(self, ctx: commands.Context, trigger: str):
+        """Add trigger to Boconomicon"""
+        async with self.config.guild(ctx.guild).triggers() as trigger_list:
+            trigger_list.append(trigger)
+        await ctx.send("Trigger `{0}` successfully added.".format(trigger))
+        await self.generate_cache()
+
+    @boconomiconset_trigger.command(name="remove")
+    async def boconomiconset_trigger_remove(self, ctx: commands.Context, trigger: str):
+        """Remove trigger from Boconomicon"""
+        async with self.config.guild(ctx.guild).triggers() as trigger_list:
+            try:
+                trigger_list.remove(trigger)
+                await ctx.send("Trigger `{0}` successfully removed.".format(trigger))
+            except ValueError:
+                await ctx.send("Trigger `{0}` is already not a trigger.".format(trigger))
+        await self.generate_cache()
+
+    @boconomiconset.group(name="override", alias=["overrides"])
+    async def boconomiconset_override(self, ctx: commands.Context):
+        """Add or remove overrides for Boconomicon"""
+        if not ctx.invoked_subcommand:
+            embed = discord.Embed(color=await ctx.embed_color(), title="Current overrides")
+            msg = ""
+            overrides = await self.config.guild(ctx.guild).overrides()
+            for word, override in overrides.items():
+                msg += "`{0}`: `{1}`\n".format(word, override)
+            embed.description = msg
+            await ctx.send(embed=embed)
+
+    @boconomiconset_override.command(name="add", alias=["edit"])
+    async def boconomiconset_override_add(self, ctx: commands.Context, keyword: str, replacement: str):
+        """Add or edit override to Boconomicon"""
+        async with self.config.guild(ctx.guild).overrides() as override_list:
+            override_list[keyword] = replacement
+        await ctx.send("`{0}` will now be replaced by `{1}`.".format(keyword, replacement))
+        await self.generate_cache()
+
+    @boconomiconset_override.command(name="remove")
+    async def boconomiconset_override_remove(self, ctx: commands.Context, keyword: str):
+        """Remove override from Boconomicon"""
+        async with self.config.guild(ctx.guild).overrides() as override_list:
+            try:
+                del override_list[keyword]
+                await ctx.send("Override `{0}` successfully removed.".format(keyword))
+            except KeyError:
+                await ctx.send("Override `{0}` is already not an override.".format(keyword))
+        await self.generate_cache()
